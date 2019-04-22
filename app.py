e307c8f1f2fdc373561d04437efb7fdb36f16fc3
abbkrish <abbkrish@gmail.com>
2019-04-22 11:52:22 -0500

    Saving reg details to Db

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, Session
 from datetime import datetime
 from RegistrationForm import RegistrationForm
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -12,13 +13,31 @@ def hello():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.username.data, form.email.data,
-                    form.password.data)
+    if request.method == 'POST':
+        #user = User(form.username.data, form.email.data)
+        try:
+            username = form.username.data
+            email = form.email.data
+            with sql.connect("pythonsqlite.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO User (username,email) VALUES (?, ?)",(username, email) )
+                con.commit()
+                print("inserted into table")
+                msg = "Record successfully added"
+
+
+        except:
+            print("Connection Error")
+            con.rollback()
+            msg = "error in insert operation"
+
         #db_session.add(user)
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 if __name__ == "__main__":
+    sess = Session()
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run()
