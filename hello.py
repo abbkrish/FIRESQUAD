6dfa977c7a1a2a59cddd6db58f434124b72ca363
abbkrish <abbkrish@gmail.com>
2019-04-22 09:39:32 -0500

    Adding time

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! " + str(datetime.now())
 
 
 if __name__ == "__main__":
