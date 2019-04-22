e31441616da288bab922981c523fa36a10ca05a6
Yunhan Zou <harryzou1003@gmail.com>
2019-04-22 10:29:55 -0500

    name changed

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World! <br>" + str(datetime.now())
+
+
+if __name__ == "__main__":
+    app.run()
