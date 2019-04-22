932dbef78e04f38d1fe7faef7ee6c56d02661636
abbkrish <abbkrish@gmail.com>
2019-04-22 11:25:37 -0500

    Dummy registration form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash, request
 from datetime import datetime
+from RegistrationForm import RegistrationForm
 
 app = Flask(__name__)
 
@@ -8,6 +9,16 @@ app = Flask(__name__)
 def hello():
     return "Hello World! <br>" + str(datetime.now())
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        #db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == "__main__":
     app.run()
