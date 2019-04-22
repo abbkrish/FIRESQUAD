2ae9ab7818f2485c150a8e7f3566eecab8848fb3
Feng <shawn101002@gmail.com>
2019-04-22 10:41:46 -0400

    add a br

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! " + str(datetime.now())
+    return "Hello World! <br>" + str(datetime.now())
 
 
 if __name__ == "__main__":
